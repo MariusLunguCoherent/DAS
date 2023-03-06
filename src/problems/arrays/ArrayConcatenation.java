849d1c3c7fe585aea865de3ed250b71f89a2b4ca
marius.lungu <marius.lungu@intradiem.com>
2023-03-06 13:22:49 +0200

    +new problem

---
+++ src/problems/arrays/ArrayConcatenation.java
@@ -0,0 +1,38 @@
+package problems.arrays;
+
+import java.util.Arrays;
+/**
+ * Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and
+ * ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+ * Specifically, ans is the concatenation of two nums arrays.
+
+ * Return the array ans.
+ * */
+public class ArrayConcatenation
+{
+
+    public static void main(String[] args)
+    {
+        int[] nums = {1, 2, 3};
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+    }
+
+    public static int[] getConcatenation(int[] nums)
+    {
+        int[] array = new int[nums.length*2];
+        int position = 0;
+        int counter = 0;
+        while(counter < 2){
+            for (int num : nums)
+            {
+                array[position] = num;
+                position++;
+            }
+            counter++;
+        }
+
+
+        return array;
+    }
+}
